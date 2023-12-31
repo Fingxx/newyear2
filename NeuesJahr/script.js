d58443aee262f693079d26aa8561567b4c2a4b5d
Fingxx <155091210+Fingxx@users.noreply.github.com>
2023-12-31 01:16:42 +0100

    Add files via upload

---
+++ NeuesJahr/script.js
@@ -0,0 +1,10 @@
+function zeigeNeujahrsgruesse() {
+    // Verstecke den Button
+    document.getElementById('neujahrButton').style.display = 'none';
+
+    // Zeige die Neujahrsnachricht und Raketen
+    var neujahrsNachricht = document.getElementById('neujahrsNachricht');
+    neujahrsNachricht.style.display = 'block';
+
+    // Optional: Fügen Sie weitere Aktionen hinzu, z. B. das Abspielen von Soundeffekten
+}
