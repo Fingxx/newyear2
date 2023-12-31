dda2653730a16f41278eacaeeba622af45a71bc5
Fingxx <155091210+Fingxx@users.noreply.github.com>
2023-12-31 01:16:26 +0100

    Delete ProjectNewYear directory

--- ProjectNewYear/newyear/script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-document.getElementById('showGreetingsButton').addEventListener('click', function() {
-    var greetingsContainer = document.createElement('div');
-    greetingsContainer.innerHTML = '<h1>Frohes Neues Jahr!</h1><p>Herzliche Neujahrsgrüße für ein glückliches und erfolgreiches Jahr!</p>';
-    document.body.appendChild(greetingsContainer);
-    document.getElementById('showGreetingsButton').style.display = 'none';
-});
